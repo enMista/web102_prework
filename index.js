cad9dc20d525571accbf1461821b3f8782247037
bolarinnnwa <oluwa.damilola246@gmail.com>
2025-08-21 13:31:47 -0400

    challenge 5 done

--- index.js
+++ index.js
@@ -120,9 +120,13 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
+    let listOfUnfundedGames = GAMES_JSON.filter( (GAMES_JSON) => {
+        return GAMES_JSON.pledged < GAMES_JSON.goal;
+    })
 
 
     // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(listOfUnfundedGames); 
 
 }
 
@@ -131,10 +135,13 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
+    let listOfFundedGames = GAMES_JSON.filter( (GAMES_JSON) => {
+        return GAMES_JSON.pledged >= GAMES_JSON.goal; 
+    })
 
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(listOfFundedGames);
 }
 
 // show all games
@@ -142,6 +149,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -151,6 +159,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 
 /*************************************************************************************
